7acb4902d437f2b3b0a0b92c20b3f61f27f83387
bruno vieira <brvieirasf@gmail.com>
2025-02-12 21:10:24 -0300

    Adding song recommendations for joy

---
+++ src/api/registry.py
@@ -0,0 +1,45 @@
+JOY_RECOMENDATIONS = {
+    "Odessa - Caribou": "https://www.youtube.com/watch?v=Yq_tDOFU5tY",
+    "Feel It Still - Portugal. The Man": "https://www.youtube.com/watch?v=e-2aoz_Um10",
+    "Good Feeling : Flo Rida": "https://www.youtube.com/watch?v=3OnnDqH6Wj8",
+    "Can't Stop the Feeling! - Justin Timberlake": "https://www.youtube.com/watch?v=ru0K8uYEZWw",
+    "Riptide - Vance Joy": "https://www.youtube.com/watch?v=uJ_1HMAGb4k",
+    "Audacia - BADBADNOTGOOD": "https://www.youtube.com/watch?v=F1VMAsSyf3w",
+    "Jorge Ben Jor - Os alquimistas estão chegando": "https://www.youtube.com/watch?v=0y1kGcMi_l8",
+    "September - Earth, Wind & Fire": "https://www.youtube.com/watch?v=Gs069dndIYk",
+    "All Star - Smash Mouth": "https://www.youtube.com/watch?v=L_jWHffIx5E",
+    "Little Talks - Of Monsters and Men": "https://www.youtube.com/watch?v=ghb6eDopW8I",
+    "Hey Ya! - Outkast": "https://www.youtube.com/watch?v=PWgvGjAhvIw",
+    "Never Gonna Give You Up - Rick Astley": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "The 7th Element - Vitas": "https://www.youtube.com/watch?v=989-7xsRLR4",
+    "Pagode Russo - Luiz Gonzaga": "https://www.youtube.com/watch?v=IAWkTqqTZOw",
+    "YA YA - Beyonce" : "https://www.youtube.com/watch?v=Ma8X2iMdv8U",
+    "Stolen Dance - Milky Chance": "https://www.youtube.com/watch?v=iX-QaNzd-0Y",
+    "Treasure - Bruno Mars": "https://www.youtube.com/watch?v=nPvuNsRccVw",
+    "Johnny B. Goode - Chuck Berry" : "https://www.youtube.com/watch?v=Y-9Y4CCIWnM",
+    "Espresso - Sabrina Carpenter" : "https://www.youtube.com/watch?v=eVli-tstM5E",
+    "Maniac (Rock version) - Carpenter Brut" : "https://www.youtube.com/watch?v=LcI8sfwxvIs",
+}
+
+SAD_RECOMMENDATIONS = {
+    "Ain't No Sunshine - Bill Withers": "https://www.youtube.com/watch?v=Nuanwn3v-2I",
+    "Hurt - Johnny Cash": "https://www.youtube.com/watch?v=8AHCfZTRGiI",
+    "Glimpse of Us - Joji" : "https://www.youtube.com/watch?v=NgsWGfUlwJI",
+    "Nude - Radiohead" : "https://www.youtube.com/watch?v=BbWBRnDK_AE",
+    "Let The Light In - Lana Del Rey ft. Father John Misty" : "https://www.youtube.com/watch?v=WJlQ4jt5Fz4",
+    "Cartola - Preciso Me Encontrar" : "https://www.youtube.com/watch?v=56mu8KSUYqk",
+    "Moonlight on the River - Mac DeMarco" : "https://www.youtube.com/watch?v=1VYbKamjB-4",
+    "O Mundo é um Moinho - Cartola" : "https://www.youtube.com/watch?v=ud9PlROstDw",
+    "As Rosas Não Falam - Cartola" : "https://www.youtube.com/watch?v=OHx8hXpqYbo",
+    "Instrument of Surrender - Sea Power" : "https://www.youtube.com/watch?v=qMUoWTEIGx4",
+    "i love you - Billie Eilish" : "https://www.youtube.com/watch?v=WiinVuzh4DA",
+    "Rain on Brick - Bill Kiley" : "https://www.youtube.com/watch?v=-9s63ZNKSzg",
+    "Oceans - Seafret" : "https://www.youtube.com/watch?v=aqsL0QQaSP4",
+    "Someone Like You - Adele" : "https://www.youtube.com/watch?v=hLQl3WQQoQ0",
+    "One More Light - Linkin Park" : "https://www.youtube.com/watch?v=Tm8LGxTLtQk",
+    # faltam 5
+}
+
+LOVE_RECOMMENDATIONS = {
+
+}
